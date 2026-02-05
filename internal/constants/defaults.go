8d7f1d4fa584d1c21d1c30ffb40876715465e051
Александр Тумайкин <AATumaykin@tsum.ru>
2026-02-05 13:39:25 +0300

    feat(constants): add defaults.go with version and build info constants
    - Added DefaultVersion, DefaultBuildTime, DefaultGitCommit, DefaultGoVersion
    - These constants will be used in cmd/nexbot/main.go to replace hardcoded values
    - Part of task Nexbot-3ev

---
+++ internal/constants/defaults.go
@@ -0,0 +1,13 @@
+package constants
+
+// DefaultVersion is the default version of the application
+const DefaultVersion = "0.1.0-dev"
+
+// DefaultBuildTime is the default build time when not provided at build time
+const DefaultBuildTime = "unknown"
+
+// DefaultGitCommit is the default git commit hash when not provided at build time
+const DefaultGitCommit = "unknown"
+
+// DefaultGoVersion is the default Go version when not provided at build time
+const DefaultGoVersion = "unknown"
